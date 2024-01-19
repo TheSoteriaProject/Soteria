ec73961c51fc412e3a592838e90021ba33f1fe98
Logan Garrett <ninetailsoftware@gmail.com>
2024-01-18 22:39:12 -0500

    ignore parser is written now just to connect them with an action

--- Sandbox/SoteriaIgnoreDemo/SoteriaIgnore.rs
+++ Sandbox/SoteriaIgnoreDemo/SoteriaIgnore.rs
@@ -1,1 +1,58 @@
+use std::fs::read_to_string;
 
+fn remove_comments_and_empty_lines(lines: Vec<String>) -> Vec<String> {
+    let mut result = Vec::new();
+
+    for line in lines {
+        let line_without_comment: String = line.splitn(2, '#').next().unwrap_or("").trim().to_string();
+
+        if !line_without_comment.is_empty() {
+            result.push(line_without_comment);
+        }
+    }
+
+    result
+}
+
+fn read_lines(filename: &str) -> Vec<String> {
+    let mut result = Vec::new();
+    let mut skip_multiline_comment = false;
+    for line in read_to_string(filename).unwrap().lines() {
+       
+        if let Some(first_char) = line.chars().next() {
+            if first_char == '#' {
+                continue;
+            }
+
+            if line.starts_with("|-start") {
+                skip_multiline_comment = true;
+                continue;
+            }
+
+            if skip_multiline_comment && line.starts_with("|-end") {
+                skip_multiline_comment = false;
+                continue;
+            }
+
+            if skip_multiline_comment {
+                continue;
+            }
+        }
+
+        result.push(line.to_string())
+    }
+    
+    let final_result = remove_comments_and_empty_lines(result);
+
+    final_result
+}
+
+
+fn main() {
+    let ignore_file = "./.soteriaignore";
+    println!("File Name: {}", ignore_file);
+
+    let content = read_lines(ignore_file);
+
+    println!("Content:\n{:?}", content);
+}
