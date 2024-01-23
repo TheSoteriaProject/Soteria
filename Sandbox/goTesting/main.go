d5c364b669b93220d4d5cbfca16e00bbb21fdefe
Logan Garrett <ninetailsoftware@gmail.com>
2024-01-22 21:05:00 -0500

    Working on Error Log

--- Sandbox/goTesting/main.go
+++ Sandbox/goTesting/main.go
@@ -3,7 +3,28 @@ package main;
 import (
 	"fmt"
 	"os"
+	"log"
 )
+
+func simulateError() error {
+	return fmt.Errorf("This is a simulated error")
+}
+
+func logError() {
+	logFile, err := os.OpenFile("error.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		panic(err)
+	}
+	defer logFile.Close()
+
+	log.SetOutput(logFile)
+	
+	// Change so you can pass in function???
+	err = simulateError()
+	if err != nil {
+		log.Println("Error:", err)
+	}
+}
 
 func help() {
 	fmt.Println("Soteria is a linter that is used for...");
@@ -18,10 +39,19 @@ func version() {
 func run(flag string) {
 	fmt.Println("Running...");
 	
+	// warning_flag := false;
+	error_flag := true;
+
 	if flag == "--warn" {
+		// warning_flag = true;
 		fmt.Println("Warning Mode: On");
 	} else {
 		fmt.Println("Warning Mode: Off");
+	}
+
+	// probably not needed here but will be used on each function
+	if error_flag == true {
+		logError();
 	}
 }
 
