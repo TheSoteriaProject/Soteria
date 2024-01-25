077503bdc4bc6e477215ff0d0c1357df44667ee9
Logan Garrett <ninetailsoftware@gmail.com>
2024-01-24 22:51:20 -0500

    Second list now compare is gonna need to happen.

--- Soteria/SoteriaIgnore/file_controller.rs
+++ Soteria/SoteriaIgnore/file_controller.rs
@@ -4,10 +4,12 @@ use std::process;
 use std::path::Path;
 use std::fs::read_to_string;
 
+// Ignore File Comment Parser
 fn remove_comments_and_empty_lines(lines: Vec<String>) -> Vec<String> {
     let mut result = Vec::new();
 
     for line in lines {
+        // Removes comments after statement
         let line_without_comment: String = line.splitn(2, '#').next().unwrap_or("").trim().to_string();
 
         if !line_without_comment.is_empty() {
@@ -18,9 +20,11 @@ fn remove_comments_and_empty_lines(lines: Vec<String>) -> Vec<String> {
     result
 }
 
+// No need to Tokenize
 fn tokenize_lines(lines: &Vec<String>) -> Vec<String> {
     let mut token_results = Vec::new();
 
+    // Adjust so it removes or keeps
     for line in lines {
         if line.starts_with("-") {
             if line.ends_with("/") {
@@ -44,6 +48,7 @@ fn tokenize_lines(lines: &Vec<String>) -> Vec<String> {
     token_results
 }
 
+// Read Lines of File
 fn read_lines(filename: &str) -> Vec<String> {
     let mut result = Vec::new();
     let mut skip_multiline_comment = false;
@@ -116,7 +121,9 @@ fn is_bourne_shell_script(file_path: &str) -> bool {
 fn is_file_ignored() {}
 */
 
-fn folder_traverse(dir: &Path) {
+fn folder_traverse(dir: &Path) -> Vec<String> {
+    let mut files = Vec::new();
+
     if dir.is_dir() {
         for entry in fs::read_dir(dir).unwrap() {
             let entry = entry.unwrap();
@@ -125,22 +132,28 @@ fn folder_traverse(dir: &Path) {
             if path.is_file() {
                 // If it's a file (not a directory) and is also one of the correct extensions
                 if let Some(path_str) = path.to_str() {
-                    if is_dockerfile(path_str) {
-                        println!("Docker File Path: {}", path.display());
+                    if is_dockerfile(path_str) { // Bad Coding
+                        // println!("Docker File Path: {}", path.display());
+                        files.push(path_str.to_owned());
                     } else if is_makefile(path_str) {
-                        println!("Makefile File Path: {}", path.display());
+                        // println!("Makefile File Path: {}", path.display());
+                        files.push(path_str.to_owned());
                     } else if is_bourne_shell_script(path_str) {
-                        println!("Bourne Shell File Path: {}", path.display());
+                        // println!("Bourne Shell File Path: {}", path.display());
+                        files.push(path_str.to_owned());
                     } else {
                         // Nothing
                     }
                 }
             } else if path.is_dir() {
                 // Recursively visit subdirectories
-                folder_traverse(&path);
+                // folder_traverse(&path);
+                files.extend(folder_traverse(&path).into_iter());
             }
         }
     }
+
+    files
 }
 
 fn main() {
@@ -157,11 +170,7 @@ fn main() {
     *********************/
     let args: Vec<String> = env::args().collect();
     if args.len() > 1 {
-        println!("Files");
-        println!("---------------------");
-        /*for arg in args.iter().skip(2) { // Skip 2 because of directory name and also program name.
-            println!("{}", arg);
-        } */
+        // DO Nothing as of now
     } else {
         process::exit(1);
     }
@@ -169,5 +178,6 @@ fn main() {
     let root_dir = args[1].clone();
     let root_path = Path::new(&root_dir);
 
-    folder_traverse(&root_path);
+    let files = folder_traverse(&root_path);
+    println!("Content:\n{:?}", files);
 }
