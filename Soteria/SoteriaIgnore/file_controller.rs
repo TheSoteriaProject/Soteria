e2c58bdec3745731d966db5a519cdb607efd667f
Logan Garrett <ninetailsoftware@gmail.com>
2024-01-24 22:23:34 -0500

    Merged Sandbox now need to correctly implement and comment

--- Soteria/SoteriaIgnore/file_controller.rs
+++ Soteria/SoteriaIgnore/file_controller.rs
@@ -2,6 +2,82 @@ use std::fs;
 use std::env;
 use std::process;
 use std::path::Path;
+use std::fs::read_to_string;
+
+fn remove_comments_and_empty_lines(lines: Vec<String>) -> Vec<String> {
+    let mut result = Vec::new();
+
+    for line in lines {
+        let line_without_comment: String = line.splitn(2, '#').next().unwrap_or("").trim().to_string();
+
+        if !line_without_comment.is_empty() {
+            result.push(line_without_comment);
+        }
+    }
+
+    result
+}
+
+fn tokenize_lines(lines: &Vec<String>) -> Vec<String> {
+    let mut token_results = Vec::new();
+
+    for line in lines {
+        if line.starts_with("-") {
+            if line.ends_with("/") {
+                token_results.push(line.to_string() + " : SkipFolder");
+            }
+            token_results.push(line.to_string() + " : SkipFile");
+        }
+
+        if line.starts_with("+") {
+            if line.ends_with("/") {
+                token_results.push(line.to_string() + " : AddFolder");
+            }
+            token_results.push(line.to_string() + " : AddFile");
+        }
+
+        if line.starts_with("*") {
+            token_results.push(line.to_string() + " : SkipExtension");
+        }
+    }
+
+    token_results
+}
+
+fn read_lines(filename: &str) -> Vec<String> {
+    let mut result = Vec::new();
+    let mut skip_multiline_comment = false;
+    for line in read_to_string(filename).unwrap().lines() {
+       
+        if let Some(first_char) = line.chars().next() {
+            if first_char == '#' {
+                continue;
+            }
+
+            if line.starts_with("|-start") {
+                skip_multiline_comment = true;
+                continue;
+            }
+
+            if skip_multiline_comment && line.starts_with("|-end") {
+                skip_multiline_comment = false;
+                continue;
+            }
+
+            if skip_multiline_comment {
+                continue;
+            }
+        }
+
+        result.push(line.to_string())
+    }
+    
+    let next_result = remove_comments_and_empty_lines(result);
+
+    let next_next_result = tokenize_lines(&next_result);
+
+    next_next_result
+}
 
 fn is_dockerfile(file_path: &str) -> bool {
     // Check if file extension is Dockerfile
@@ -68,13 +144,24 @@ fn folder_traverse(dir: &Path) {
 }
 
 fn main() {
+
+    /**************************
+    ** Gather Special Cases **
+    **************************/
+    let _ignore_file = "./.soteriaignore";
+    let content = read_lines(_ignore_file);
+    println!("Content:\n{:?}", content);
+
+    /**********************
+    ** Gather File List **
+    *********************/
     let args: Vec<String> = env::args().collect();
     if args.len() > 1 {
         println!("Files");
         println!("---------------------");
-        for arg in args.iter().skip(2) { // Skip 2 because of directory name and also program name.
+        /*for arg in args.iter().skip(2) { // Skip 2 because of directory name and also program name.
             println!("{}", arg);
-        }
+        } */
     } else {
         process::exit(1);
     }
