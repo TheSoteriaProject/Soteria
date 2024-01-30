c2f843a451aa5bda1e299345874e33936cf82c49
Logan Garrett <ninetailsoftware@gmail.com>
2024-01-30 15:58:30 -0500

    Changing naming and basically turn it into one function on file controller side.

--- Soteria/file_controller/file_controller.go
+++ Soteria/file_controller/file_controller.go
@@ -2,9 +2,26 @@ package file_controller
 
 import (
 	"fmt"
+	"os"
 )
 
-// Needs To Be Uppercase
+// Confirm File Connection
 func TestConnection() {
 	fmt.Println("Hello From File Controller.\n")
+}
+
+// Path Traversal
+func GetAllFiles(path string, info os.FileInfo, err error) error {
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+
+	if info.IsDir() {
+		fmt.Printf("Directory: %s\n", path)
+	} else {
+		fmt.Printf("File: %s\n", path)
+	}
+
+	return nil
 }
--- Soteria/main.go
+++ Soteria/main.go
@@ -5,48 +5,27 @@ import (
 	"errors"
 	// "os/exec"
 	"os"
+	"path/filepath"
 
 	// Custom Files
 	"Soteria/file_controller"
 )
 
-/* 
-// File Controller to deal with sorting files
-func file_controller(filePath string) {
-	// Exact Path to file controller
-	execPath, err := exec.LookPath("./SoteriaIgnore/file_controller")
-
-        if err != nil {
-                fmt.Println("Error: ", err)
-        } else {
-		// Run Rust Executable
-                cmd := exec.Command(execPath, filePath)
-		
-		// Show Output
-                cmd.Stdout = os.Stdout
-                cmd.Stderr = os.Stderr
-
-                // Run & Check for Error
-		cmdErr := cmd.Run()
-                if cmdErr != nil {
-                        panic(cmdErr)
-                }
-        }
-}
-*/
 func main() {
 	fmt.Println("Welcome to Insecure Communication Linter.")
-	
 	file_controller.TestConnection()
 
 	// Check Length
 	if len(os.Args) > 1 {
 		// Grab file Path from CL Arg
-		filePath := os.Args[1]
+		file_path := os.Args[1]
 
 		// Check if Exist
-		if _, err := os.Stat(filePath); err == nil {
-			// file_controller(filePath) // Gathers Files	
+		if _, err := os.Stat(file_path); err == nil {
+			filepath.Walk(file_path, file_controller.GetAllFiles)
+			if err != nil {
+				fmt.Printf("error gathering path %v: %v\n", file_path, err)
+			}
 		} else if errors.Is(err, os.ErrNotExist) {
   			// Path Does Not Exist
 			// Add Error
