de74533a0054e6d621063e1f576ae59303215cc3
Logan Garrett <ninetailsoftware@gmail.com>
2024-01-31 23:35:30 -0500

    Setting Up For Tomorrow

--- Soteria/file_controller/ignore_file_parser.go
+++ Soteria/file_controller/ignore_file_parser.go
@@ -1,1 +1,23 @@
-// Adjust Name Later if Needed
+package ignore_file_parser
+
+import (
+	"fmt"
+	"os"
+)
+
+
+func RemoveOneLineComments() {}
+
+func RemoveMultiLineComments() {}
+
+func Tokenize() {}
+
+func IgnoredFiles() []string {
+	// Remove Comments
+
+	// Remove Multi-Line Comments
+
+	// Tokenize Files and Folders for Include and Not Include
+
+	// Return Slice
+}
