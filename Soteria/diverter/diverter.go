b45d5839068bdc675e6df0abf81f2b53b20d14f0
NineTailSecurity <ninetailsoftware@gmail.com>
2024-02-04 00:21:07 -0500

    Fixing Package Issue

--- Soteria/diverter/diverter.go
+++ Soteria/diverter/diverter.go
@@ -1,2 +1,8 @@
 // Possibly Best To Keep Seperate due to Return Codes and assumed flags?????
 // Uncertain will think about at a later point
+// can't leave file blank
+package diverter
+
+func divert() {
+	// Do Nothing
+}
--- Soteria/lexers/bash_analyzer.go
+++ Soteria/lexers/bash_analyzer.go
@@ -1,1 +1,8 @@
 // Just Setting Up
+// cannot leave blank
+
+package bash_analyzer
+
+func lex() {
+	// Do Nothing
+}
