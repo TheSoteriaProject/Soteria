a602419c4a85b7210cacda103d06bc4fb62bfaec
NineTailSecurity <ninetailsoftware@gmail.com>
2024-02-08 21:46:06 -0500

    Regex Match for first file test

--- Soteria/bash_analyzer/bash_analyzer.go
+++ Soteria/bash_analyzer/bash_analyzer.go
@@ -1,7 +1,43 @@
 package bash_analyzer
 
-func GetVariables(file string) []string {
+import (
+	"os"
+	"fmt"
+	"bufio"
+	"regexp"
+)
+
+func GetVariables(file_name string) []string {
 	variable_list := []string{}
+	
+	file, err := os.Open(file_name)
+	if err != nil {
+		fmt.Println("Error Opening: ", err)
+		return nil 
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		// check regex
+		// grab line number ???
+		regex_pattern := `\b([a-zA-Z_][a-zA-Z0-9_]*)\s*=\s*`
+		regex_expression := regexp.MustCompile(regex_pattern)
+		matches := regex_expression.FindAllStringSubmatch(line, -1)
+		
+		for _, match := range matches {
+			if len(match) > 1 {
+				variable_list = append(variable_list, match[1])
+			}
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error Reaading: ", err)
+	}
+
 	return variable_list
 }
 
--- Soteria/bash_analyzer/bash_analyzer_test.go
+++ Soteria/bash_analyzer/bash_analyzer_test.go
@@ -4,31 +4,42 @@ import (
 	"os"
 	"reflect"
 	"testing"
+	"path/filepath"
 	"Soteria/bash_analyzer"
 )
 
 // Checks if it picks up deined variable defnitions. This can be checked based on all captiols.
 func TestGetVariables(t *testing.T) {
+	cwd, err := os.Getwd()
+    	if err != nil {
+        	t.Fatalf("Error getting current working directory: %v", err)
+    	}
+
 	// Test File
-	file := "/Soteria/Files/sample_data/curl_examples.sh"
+	test_file := "../../Files/sample_data/curl_examples.sh"
 	
+	// Construct the file path
+    	file := filepath.Join(cwd, test_file)
+
 	// Fucntion Call
 	variable_list := bash_analyzer.GetVariables(file)
 
-	// Test Data
-	// Gonna have to deal with POST since it is not a variable but is used so reserved list??
+	// Test Data Variable define list
 	test_list := []string{"DOWNLOAD_URL", 
 			      "POST_URL", 
-			      "POST_DATA",
+			      "POST_DATA", 
+			      "param1", 
+			      "param2",
 			      "CURL",
 			      "INSECURE", 
 			      "DATA",
 		      	      "HEADER", 
 			      "REQUEST", 
 			      "OUTPUT", 
-			      "INSECURE_CURL"}
+			      "INSECURE_CURL", 
+			      "command", 
+			      "command"}
 
-	// local variables will be a pain
 
 	// Compare
         if !reflect.DeepEqual(test_list, variable_list) {
