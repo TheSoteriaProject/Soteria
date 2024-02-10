ca9af60708001c1f2f7ec62e29a998a79a2794ce
NineTailSecurity <ninetailsoftware@gmail.com>
2024-02-09 23:43:34 -0500

    Testing Variable grab is okay. Some cases may need to be adjusted for second runs.

--- Soteria/bash_analyzer/bash_analyzer.go
+++ Soteria/bash_analyzer/bash_analyzer.go
@@ -41,6 +41,50 @@ func GetVariables(file_name string) []string {
 	return variable_list
 }
 
+func GetVariableDefinitions(file_name string) []string {
+	definition_list := []string{}
+	file, err := os.Open(file_name)
+        if err != nil {
+                fmt.Println("Error Opening: ", err)
+                return nil
+        }
+
+        defer file.Close()
+
+        scanner := bufio.NewScanner(file)
+        for scanner.Scan() {
+                line := scanner.Text()
+                // check regex
+                // grab line number ???
+                regex_pattern := `(=)(.*)`
+                regex_expression := regexp.MustCompile(regex_pattern)
+                matches := regex_expression.FindAllStringSubmatch(line, -1)
+
+                for _, match := range matches {
+                        if len(match) > 1 {
+                                definition_list = append(definition_list, match[2])
+                        }
+                }
+
+		// Second Pass
+		regex_pattern = `\b=([a-zA-Z_\/:-][a-zA-Z0-9_\/:-]*)`
+		regex_expression = regexp.MustCompile(regex_pattern)
+                matches2 := regex_expression.FindAllStringSubmatch(line, -1)
+
+                for _, match2 := range matches2 {
+                        if len(match2) > 1 {
+                                definition_list = append(definition_list, match2[1])
+                        }
+                }
+        }
+
+        if err := scanner.Err(); err != nil {
+                fmt.Println("Error Reaading: ", err)
+        }
+	
+	return definition_list
+}
+
 func Main() {
 	// Dummy Pass in for now
 	GetVariables("./nothing.txt")
--- Soteria/bash_analyzer/bash_analyzer_test.go
+++ Soteria/bash_analyzer/bash_analyzer_test.go
@@ -9,7 +9,7 @@ import (
 )
 
 // Checks if it picks up deined variable defnitions. This can be checked based on all captiols.
-func TestGetVariables(t *testing.T) {
+func TestGetVariables1(t *testing.T) {
 	cwd, err := os.Getwd()
     	if err != nil {
         	t.Fatalf("Error getting current working directory: %v", err)
@@ -47,10 +47,122 @@ func TestGetVariables(t *testing.T) {
     	}
 }
 
+// Checks if it picks up deined variable defnitions. This can be checked based on all captiols.
+func TestGetVariables2(t *testing.T) {
+        cwd, err := os.Getwd()
+        if err != nil {
+                t.Fatalf("Error getting current working directory: %v", err)
+        }
+
+        // Test File
+        test_file := "../../Files/sample_data/ssh_examples.sh"
+        
+        // Construct the file path
+        file := filepath.Join(cwd, test_file)
+
+        // Fucntion Call
+        variable_list := bash_analyzer.GetVariables(file)
+
+        // Test Data Variable define list
+        test_list := []string{"ssh_opts",
+			      "StrictHostKeyChecking", 
+			      "UserKnownHostsFile",
+                              "ssh_cmd", 
+                              "StrictHostKeyChecking", 
+                              "UserKnownHostsFile"}
+
+        // Compare
+        if !reflect.DeepEqual(test_list, variable_list) {
+                t.Errorf("Expected: %v, Got: %v", test_list, variable_list)
+        }
+}
+
+// Checks if it picks up deined variable defnitions. This can be checked based on all captiols.
+func TestGetVariables3(t *testing.T) {
+        cwd, err := os.Getwd()
+        if err != nil {
+                t.Fatalf("Error getting current working directory: %v", err)
+        }
+
+        // Test File
+        test_file := "../../Files/sample_data/wget_examples.sh"
+        
+        // Construct the file path
+        file := filepath.Join(cwd, test_file)
+
+        // Fucntion Call
+        variable_list := bash_analyzer.GetVariables(file)
+
+        // Test Data Variable define list
+        test_list := []string{"DOWNLOAD_URL", 
+                              "POST_URL", 
+                              "POST_DATA", 
+                              "param1", 
+                              "param2",
+                              "WGET",
+                              "NO_CHECK_CERTIFICATE", 
+                              "POST",
+                              "HEADER", 
+			      "header", 
+                              "data", // sus????
+                              "NO_CHECK_CERTIFICATE_WGET", 
+                              "command", 
+                              "command"}
+
+
+        // Compare
+        if !reflect.DeepEqual(test_list, variable_list) {
+                t.Errorf("Expected: %v, Got: %v", test_list, variable_list)
+        }
+}
+
+func TestGetVariableDefinitions1(t *testing.T) {
+	cwd, err := os.Getwd()
+        if err != nil {
+                t.Fatalf("Error getting current working directory: %v", err)
+        }
+
+        // Test File
+        test_file := "../../Files/sample_data/curl_examples.sh"
+        
+        // Construct the file path
+        file := filepath.Join(cwd, test_file)
+
+        // Fucntion Call
+        definition_list := bash_analyzer.GetVariableDefinitions(file)
+
+        // Test Data Definition define list
+        test_list := []string{"'https://example.com/installer.pkg'", 
+                              "'https://example.com/api/endpoint'", 
+                              "'param1=value1&param2=value2'", 
+                              "value1", 
+                              "value2",
+                              "'curl'",
+                              "'--insecure'", 
+                              "'--data'",
+                              "'--header \"Content-Type: application/x-www-form-urlencoded\"'", 
+                              "'--request'", 
+                              "'--output'", 
+                              "\"${CURL} ${INSECURE}\"", 
+                              "$1", 
+                              "('curl' '-k' '-o' 'installer3.pkg' \"${DOWNLOAD_URL}\")"}
+
+
+        // Compare
+        if !reflect.DeepEqual(test_list, definition_list) {
+                t.Errorf("Expected: %v, Got: %v", test_list, definition_list)
+        }
+}
+
 func TestingController(t *testing.T) { 
         // TestConnections()
-	TestGetVariables(t)
-}       
+	TestGetVariables1(t)
+	TestGetVariableDefinitions1(t)
+	TestGetVariables2(t)
+        // TestGetVariableDefinitions2(t)
+	TestGetVariables3(t)
+        // TestGetVariableDefinitions3(t)
+}
 
 func MainTest(m *testing.M) {
         os.Exit(m.Run())
