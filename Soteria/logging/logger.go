921edb83cb5e90df9cad851ca744154559274238
LoganG03 <ninetailsoftware@gmail.com>
2024-02-15 19:59:26 -0500

    Logging Ready?

--- Soteria/logging/logger.go
+++ Soteria/logging/logger.go
@@ -3,6 +3,7 @@ package logger
 import (
 	"encoding/json"
 	"fmt"
+	"os"
 )
 
 type Log struct {
@@ -12,16 +13,18 @@ type Log struct {
 	ErrorType  string
 }
 
-func StoreJsonLogs(log *Log) {
-	// Not Implemented
-	// Stor Logs in File??
-	fmt.Println("Stored Logs: ", log)
+func StoreJsonLogs(log_data []byte) {
+	err := os.WriteFile("../../logs/log.json", log_data, 0644)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 }
 
 func JsonLogger(LineNumber int, Variable string, Definition string, ErrorType string) {
 	log := &Log{LineNumber: LineNumber, Variable: Variable, Definition: Definition, ErrorType: ErrorType}
-	StoreJsonLogs(log)
 	printLog, err := json.MarshalIndent(log, "", "\t")
+	StoreJsonLogs(printLog)
 	if err != nil {
 		panic(err)
 	}
