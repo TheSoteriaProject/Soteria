c191eeee9789682f3eaaaa095a89800ac4835fb1
Logan <ninetailsoftware@gmail.com>
2024-02-20 17:20:08 -0500

    Loggging to a file is working and so it terminal logging

--- Soteria/logging/logger.go
+++ Soteria/logging/logger.go
@@ -9,17 +9,54 @@ import (
 type Log struct {
 	FileName   string
 	LineNumber int
-	Variable   string
-	Definition string
+	Line       string
+	Issue      string
 	ErrorType  string
 }
 
 func StoreJsonLogs(log_data Log) {
-	// Not Implemnted
+	file, err := os.OpenFile("../logs/log.json", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
+
+	existingData, err := os.ReadFile("../logs/log.json")
+	if err != nil {
+		fmt.Println("Error reading JSON from file:", err)
+		return
+	}
+
+	if len(existingData) > 0 {
+		existingData = existingData[:len(existingData)-1]
+	}
+
+	if len(existingData) == 0 {
+		existingData = append(existingData, '[')
+	}
+
+	if len(existingData) > 1 {
+		existingData = append(existingData, ',')
+	}
+
+	jsonData, err := json.Marshal(log_data)
+	if err != nil {
+		fmt.Println("Error marshaling JSON:", err)
+		return
+	}
+
+	jsonData = append(existingData, jsonData...)
+	jsonData = append(jsonData, ']')
+
+	if err := os.WriteFile("../logs/log.json", jsonData, 0644); err != nil {
+		fmt.Println("Error writing JSON to file:", err)
+		return
+	}
 }
 
-func JsonLogger(FileName string, LineNumber int, Variable string, Definition string, ErrorType string) {
-	log := &Log{FileName: FileName, LineNumber: LineNumber, Variable: Variable, Definition: Definition, ErrorType: ErrorType}
+func JsonLogger(FileName string, LineNumber int, Line string, Issue string, ErrorType string) {
+	log := &Log{FileName: FileName, LineNumber: LineNumber, Line: Line, Issue: Issue, ErrorType: ErrorType}
 	printLog, err := json.MarshalIndent(log, "", "\t")
 	if err != nil {
 		fmt.Println("Error marshalling log:", err)
@@ -28,7 +65,7 @@ func JsonLogger(FileName string, LineNumber int, Variable string, Definition str
 	fmt.Printf("%s\n", printLog)
 
 	// Store log data
-	// StoreJsonLogs(*log)
+	StoreJsonLogs(*log)
 }
 
 // Not Implemented
