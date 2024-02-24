381f927df52b24c074764a96b464dfbe44edebd7
Logan <ninetailsoftware@gmail.com>
2024-02-23 23:41:32 -0500

    Adjust File Name and add some func info.

--- Soteria/logging/logger.go
+++ Soteria/logging/logger.go
@@ -14,15 +14,16 @@ type Log struct {
 	ErrorType  string
 }
 
+// StoreJsonLogs takes in the JSON data and writes it to a file so it can be read.
 func StoreJsonLogs(log_data Log) {
-	file, err := os.OpenFile("../logs/log.json", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
+	file, err := os.OpenFile("../logs/bash_log.json", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
 	if err != nil {
 		fmt.Println("Error opening file:", err)
 		return
 	}
 	defer file.Close()
 
-	existingData, err := os.ReadFile("../logs/log.json")
+	existingData, err := os.ReadFile("../logs/bash_log.json")
 	if err != nil {
 		fmt.Println("Error reading JSON from file:", err)
 		return
@@ -49,12 +50,13 @@ func StoreJsonLogs(log_data Log) {
 	jsonData = append(existingData, jsonData...)
 	jsonData = append(jsonData, ']')
 
-	if err := os.WriteFile("../logs/log.json", jsonData, 0644); err != nil {
+	if err := os.WriteFile("../logs/bash_log.json", jsonData, 0644); err != nil {
 		fmt.Println("Error writing JSON to file:", err)
 		return
 	}
 }
 
+// Json Logger handles json in take, prints,  and sends it off to be store in file.
 func JsonLogger(FileName string, LineNumber int, Line string, Issue string, ErrorType string) {
 	log := &Log{FileName: FileName, LineNumber: LineNumber, Line: Line, Issue: Issue, ErrorType: ErrorType}
 	printLog, err := json.MarshalIndent(log, "", "\t")
@@ -68,14 +70,18 @@ func JsonLogger(FileName string, LineNumber int, Line string, Issue string, Erro
 	StoreJsonLogs(*log)
 }
 
-// Not Implemented
+// DestroyJsonLog truncates already made file and deletes log(s)
 func DestroyJsonLog() error {
-	filename := "../logs/log.json"
-	if err := os.Truncate(filename, 0); err != nil {
-		return err
-	}
-	if err := os.Remove(filename); err != nil {
-		return err
+	filename := "../logs/bash_log.json"
+	if _, err := os.Stat(filename); !os.IsNotExist(err) {
+		fmt.Printf("File '%s' does not exist\n", filename)
+
+		if err := os.Truncate(filename, 0); err != nil {
+			return err
+		}
+		if err := os.Remove(filename); err != nil {
+			return err
+		}
 	}
 	return nil
 }
