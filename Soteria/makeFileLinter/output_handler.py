01424bac5e06ec26000607c23ee19cd38ab157a1
NineTailSecurity <ninetailsoftware@gmail.com>
2024-04-11 00:05:17 -0400

    Adding comments I need to rememeber

--- Soteria/makeFileLinter/output_handler.py
+++ Soteria/makeFileLinter/output_handler.py
@@ -5,3 +5,4 @@ def write_issues_to_json(issues, filename):
     with open(filename, 'a+') as file:
         json.dump(issues, file, indent=4)
         # Dumping Wrong. Need to adjust this so it is [{}, {}, {}] instead of [{}] [{}] [{}]
+        # Also Should it be W+ or A+? I can't remember will need to test when adding the truncate to the file before each run.
